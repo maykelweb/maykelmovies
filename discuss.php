27cd71e6c8d70c1ba885f3e94677272cb953bcfd
Maykel Delgado <maykeld@outlook.com>
2021-11-30 18:42:54 +0200

    working on reply form, about to try a new way

--- discuss.php
+++ discuss.php
@@ -1,4 +1,13 @@
-<?php 
+<?php
+//Redirect to homepage if no post id given
+if (empty($_GET['id'])) {
+    //NO POSTS TO SHOW
+    header("location: index.php");
+    die();
+}
+
+$id = $_GET['id'];
+
 // Include config file
 require_once "config.php";
 
@@ -6,6 +15,49 @@ require_once "config.php";
 if (session_id() == "")
   session_start();
 
+// Processing form data when reply is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+  //Check User is logged in to reply
+  if ($_SESSION["loggedin"] == true) {
+    //Validate content
+    if(empty(trim($_POST["reply"]))){
+      $content_err = "You must add content before you can submit post.";    
+    } else {
+      $content = trim($_POST["reply"]);
+    }
+
+  // Check input errors before inserting in database
+  if(empty($content_err)){
+        
+    // Prepare an insert statement
+    $sql = "INSERT INTO replies (reply, reply_user, reply_post, parent_reply_id) VALUES (?, ?, ?, ?)";
+
+    if($stmt = mysqli_prepare($link, $sql)) {
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "siii", $param_reply, $param_reply_user, $param_reply_post, $param_parent_reply_id);
+        
+        // Set parameters
+        $param_reply = $content;
+        $param_reply_user = $_SESSION["id"];
+        $param_reply_post = $id;
+        $param_parent_reply_id = null;
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+          
+        } else{
+            echo '<div class="alert alert-danger"> Oops! Something went wrong. Please try again later. </div>';
+        }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
+        header( "Location: {$_SERVER['REQUEST_URI']}", true, 303 );
+    }
+  }
+
+  }
+}
 require_once "header.php"; ?>
 
 <body>
@@ -26,7 +78,9 @@ require_once "header.php"; ?>
             <div class="card posts">
                 <div class="card-body"> 
                     <h1>'. $row['title'] .' </h1>
-                    <p class="card-subtitle font-italic">by: '. $row['username'] .'</p>
+                    <a href="profile.php?id='. $row['user_id'] .'">
+                      <p class="card-subtitle font-italic">by: '. $row['username'] .'</p>
+                    </a>
                     <p class="card-text mt-3">'. $row['content'] .'</p>
                     <p class="card-subtitle float-right mt-3">Posted '. $row['time_created'] .'</h1>
                 </div>
@@ -66,7 +120,8 @@ require_once "header.php"; ?>
           </div>
         </div>
     </section>
-
+    
+    <!-- This is the replies part -->
     <section id="thread" class="mb-4">
         <div class="container">
           <?php
@@ -76,19 +131,22 @@ require_once "header.php"; ?>
               //No Replies
             } else {
               //Display Replies
+              $count = 0;
               while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
               echo '
-                <div class="card posts">
+                <div class="card posts" style="order:'.$count.'">
                   <div class="card-body"> 
-                    <p class="card-subtitle font-italic">by: '. $row['username'] .'</p>
+                    <a href="profile.php?id='. $row['user_id'] .'">
+                      <p class="card-subtitle font-italic">by: '. $row['username'] .'</p>
+                    </a>
                     <p class="card-text mt-3"> '. $row['reply'] .'</p>
                     <p class="card-subtitle float-right mt-3">Posted '. $row['reply_time'] .'</p> 
-                    <a href="">
+                    <a id="user'. $row['user_id'] .'" class="replyButton">
                       <span class="float-left d-inline-block" style="font-size:1.4em;"> <i class="fas fa-comment mr-2"></i>Reply</span>
                     </a>
                   </div>
                 </div>
-              ';
+              '; $count++;
               }
             }
             
@@ -97,9 +155,72 @@ require_once "header.php"; ?>
               echo '<div class="alert alert-danger"> Oops! Something went wrong. Please try again later. </div>';
             }
           ?>
+
+          <!-- Reply Form -->
+          <form action="" id="" method="post" class="mt-3">
+            <div class="form-group">
+              <label>Post a reply</label>
+              <textarea id="editor" type="textarea" name="reply" rows="10" style="height:100%;" class="form-control <?php echo (!empty($content_err)) ? 'is-invalid' : ''; ?>" value=""></textarea>
+              <span class="post-invalid invalid-feedback"><?php echo $content_err; ?></span>
+            </div>    
+            <div class="form-group">
+              <input type="submit" class="btn btn-primary btn-block" value="Post">
+            </div>
+          </form>
         </div>
     </section>
+
+    <script>
+      window.onload = () => {
+        //Add onclick event for all reply buttons
+        let replyButtons = document.querySelectorAll('.replyButton')
+
+        for (var i = 0; i < replyButtons.length; i++) {
+          replyButtons[i].addEventListener('click', (e) => {
+            //Going back 3 elements to get parent div
+            parentDiv = e.target.parentElement;
+            for (var i = 0; i < 2; i++) {
+              parentDiv = parentDiv.parentElement;
+            }
+            
+            /*Create form to insert
+            var replyForm = '
+            <form action="" id="" method="post" class="mt-3">' +
+            '<div class="form-group">' +
+              '<label>Post a reply</label>' +
+              '<textarea id="editor" type="textarea" name="reply" rows="10" style="height:100%;" class="form-control ' + '<' + '?php echo (!empty($content_err)) ? "is-invalid" : ""; ?>" value=""></textarea>' +
+              '<span class="post-invalid invalid-feedback">' + '<' + '?php echo $content_err; ?></span>' +
+            '</div>' + 
+            '<div class="form-group">' +
+              '<input type="submit" class="btn btn-primary btn-block" value="Post">' +
+            '</div>' +
+            '</form>';
+
+            var testForm = 
+            '<form action="" id="" method="post" class="mt-3">' +
+            '<div class="form-group">' +
+              '<label>Post a reply</label>' +
+              '<textarea id="editor" type="textarea" name="reply" rows="10" style="height:100%;" class="form-control"> </textarea>' + 
+            '</div>' +
+            '<div class="form-group">' +
+              '<input type="submit" class="btn btn-primary btn-block" value="Post">' +
+            '</div>' +
+            '</form>';
+            
+            parentDiv.insertAdjacentHTML('afterend', testForm);
+            */
+          })
+        }
+      }
+    </script>
 
 <?php
 require_once("footer.php");
 ?>
+
+<style>
+  #thread .container {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
